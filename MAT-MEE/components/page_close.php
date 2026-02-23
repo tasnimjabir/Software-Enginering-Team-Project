281118eb1cc378de8005fd1130d796babafa1d42
Tasnim Jabir <tasnimjabircse@gmail.com>
2026-02-23 00:35:50 +0000

    Component added

---
+++ MAT-MEE/components/page_close.php
@@ -0,0 +1,32 @@
+<?php require_once 'components/config-component.php'; ?>
+<!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h6>About MAT MEE</h6>
+                    <p>Premium clothing for modern lifestyle.</p>
+                </div>
+                <div class="col-md-4">
+                    <h6>Quick Links</h6>
+                    <ul class="footer-links">
+                        <li><a href="#about">About Us</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h6>Contact Us</h6>
+                    <p>Email: info@matmee.com<br>Phone: +1 (555) 123-4567</p>
+                </div>
+            </div>
+            <hr>
+            <div class="footer-bottom">
+                <p>&copy; 2026 MAT MEE. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
